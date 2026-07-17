70336a4c10c572c19ad16d72ffe712dcd45dd110
easonlee <easonlee66@qq.com>
2026-07-17 14:44:37 +0800

    complete core.js

--- core.js
+++ core.js
@@ -1,70 +1,120 @@
 "use strict";
-var data={
+const data={
     available:["arare_salute.jpg","arare_first.jpg"],
     character:{
-        arare:["arare_salute.jpg"]
+        arare:["arare_salute.jpg","arare_first.jpg"],
+        nonoka:[],
+        ritsu:[],
+        miyako:[],
+        yuno:[],
+        other:[]
     },
     episode:{
-        1:["arare_salute.jpg"]
+        a:["arare_salute.jpg","arare_first.jpg"],
+        b:[],
+        c:[],
+        d:[],
+        e:[],
+        other:[]
     }
 };
 function init(){
-    var show=document.getElementById("show_image");
+    const show=document.getElementById("show_image");
     var result_string="";
-    for (let i = 0; i < data.available.length; i++) {
-        result_string+=("<img src=\"img/"+data.available[i]+"\"/>");
+    for(i in data.available){
+        result_string+=("<img src=\"img/"+i+"\"/>");
     }
     show.innerHTML=result_string;
 }
 function process(){
-    var result=document.getElementById("result");
+//    const result=document.getElementById("result");
+    const show=document.getElementById("show_image");
+    const c_a=new Object();
+    const e_a=new Object();
+    for(let i of data.available){
+        c_a[i]=false;
+        e_a[i]=false;
+    }
     var result_string="";
     if(document.getElementById("e_all").selected){
-        result_string+="e all";
+        for(let i in e_a){
+            e_a[i]=true;
+        }
     }
     else if(document.getElementById("e_1").selected){
-        result_string+="e 1";
+        for(let i of data.episode.a){
+            e_a[i]=true;
+        }
     }
     else if(document.getElementById("e_2").selected){
-        result_string+="e 2";
+        for(let i of data.episode.b){
+            e_a[i]=true;
+        }
     }
     else if(document.getElementById("e_3").selected){
-        result_string+="e 3";
-    }
-    else if(document.getElementById("e_3").selected){
-        result_string+="e 3";
+        for(let i of data.episode.c){
+            e_a[i]=true;
+        }
     }
     else if(document.getElementById("e_4").selected){
-        result_string+="e 4";
+        for(let i of data.episode.d){
+            e_a[i]=true;
+        }
+    }
+    else if(document.getElementById("e_5").selected){
+        for(let i of data.episode.e){
+            e_a[i]=true;
+        }
     }
     else if(document.getElementById("e_other").selected){
-        result_string+="e other";
+        for(let i of data.episode.other){
+            e_a[i]=true;
+        }
     }
     else{
-        result_string+="e error input";
+        alert("e error input");
     }
     if(document.getElementById("arare").checked){
-        result_string+="c arare";
+        for(let i of data.character.arare){
+            c_a[i]=true;
+        }
     }
     if(document.getElementById("nonoka").checked){
-        result_string+="c nonoka";
+        for(let i of data.character.nonoka){
+            c_a[i]=true;
+        }
     }
     if(document.getElementById("ritsu").checked){
-        result_string+="c ritsu";
+        for(let i of data.character.ritsu){
+            c_a[i]=true;
+        }
     }
     if(document.getElementById("miyako").checked){
-        result_string+="c miyako";
+        for(let i of data.character.miyako){
+            c_a[i]=true;
+        }
     }
     if(document.getElementById("yuno").checked){
-        result_string+="c yuno";
+        for(let i of data.character.yuno){
+            c_a[i]=true;
+        }
     }
 //    if(document.getElementById("viola").checked){
-//        result_string+="c viola";
+//        for(let i of data.character.viola){
+//            c_a[i]=true;
+//        }
 //    }
     if(document.getElementById("other_c").checked){
-        result_string+="c other";
+        for(let i of data.character.other){
+            c_a[i]=true;
+        }
     }
-    result.innerHTML=result_string;
+    for(i in data.available){
+        if(e_a[i] && c_a[i]){
+            result_string+=("<img src=\"img/"+i+"\"/>");
+        }
+    }
+    show.innerHTML=result_string;
 }
 //function showSearch(){}
 //function showChoose(){}
